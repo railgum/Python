88ed13a064708aee00a714d2115e0734abc78d41
Rail <railbolo@gmail.com>
2022-12-20 18:15:16 +0400

    start hw3

---
+++ seminars/seminar3_tasks.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+
+inpDict = {
+    "I": "S001",
+    "II": "S002",
+    "III": "S001",
+    "IV": "S005",
+    "V": " S005 ",
+    "VI": " S009 ",
+    "VII": " S007 ",
+}
