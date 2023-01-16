5d90502bf20c8f2bb974a90d10bbb43f74321453
Rail <railbolo@gmail.com>
2023-01-16 20:29:53 +0400

    add task 108

---
+++ games/ticTacToe.py
@@ -0,0 +1,12 @@
+import candies
+
+
+# Задача 107 Создайте программу для игры в ""Крестики-нолики"" (Добавьте игру против бота)
+
+
+def tic_tac_toe(rules_tic_tac):
+    init_game.init_game(rules_tic_tac)
+
+
+rules_tic = 'Игра "Крестики-нолики"'
+tic_tac_toe(rules_tic)
